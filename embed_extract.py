9890d57f06439f63fccea81fe84a8d88c2c535b7
PecholaL <pechola.lee@outlook.com>
2023-11-23 17:32:22 +0800

    ☕️

--- embed_extract.py
+++ embed_extract.py
@@ -1,4 +1,4 @@
-""" Test the trained IDEAW
+""" Test the trained IDEAW (hard coding, only for specific test)
     * get the marked audio
     * extract the msg and compute ACC
     * ...
@@ -6,13 +6,17 @@
 
 import torch
 import torch.nn as nn
-import yaml
+import tqdm
 
 from models.ideaw import IDEAW
+from data.process import read_resample
 
-# paths
+# config paths
 config_model_path = "/Users/pecholalee/Coding/IDEAW/models/config.yaml"
 ckpt_path = "/Users/pecholalee/Coding/Watermark/ideaw_data/save/ideaw.ckpt"
+
+# audio path
+audio_path = "/Users/pecholalee/Coding/Watermark/miniAWdata/p225_008.wav"
 
 # build model and load trained parameters
 ideaw = IDEAW(config_model_path)
@@ -20,3 +24,28 @@ print("[IDEAW]model built")
 
 ideaw.load_state_dict(torch.load(ckpt_path))
 print("[IDEAW]model loaded")
+
+# generate msg and lcode
+watermark_msg = torch.randint(0, 2, (1, 16), dtype=torch.float)
+watermark_msg[watermark_msg == 0] = -1
+locate_code = torch.randint(0, 2, (1, 10), dtype=torch.float)
+
+with torch.no_grad():
+    ideaw.eval()
+
+    audio, _, _ = read_resample(audio_path=audio_path, sr=16000, audio_limit_len=None)
+    chunk_size = 16000 * (1 + 0.5)
+    chunk_num = int(len(audio) / chunk_size)
+
+    it = range(chunk_num)
+    it = tqdm.tqdm(it, desc="Embedding")
+    for i in it:
+        start_pos = i * chunk_size
+        chunk = audio[start_pos, start_pos + chunk_size].copy()
+
+        # embed msg
+        audio_wmd1, audio_wmd1_stft = ideaw.embed_msg(chunk, watermark_msg)
+        # embed lcode
+        audio_wmd2, _ = ideaw.embed_lcode(audio_wmd1_stft, locate_code)
+
+        # output the watermarked wave
