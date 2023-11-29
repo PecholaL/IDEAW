0fefa99b1154db05e6c0df1a35696c5806cd86fd
PecholaL <pechola.lee@outlook.com>
2023-11-29 17:25:57 +0800

    🥛

--- models/attackLayer.py
+++ models/attackLayer.py
@@ -6,16 +6,44 @@
     * ...
 """
 
+import numpy
+import random
 import torch
 import torch.nn as nn
 import yaml
 
+
 class AttackLayer(nn.Module):
     def __init__(self, config_path):
         super(AttackLayer, self).__init__()
-        self.load_config(config_path)
+
+    def forward(self, audio):
+        pass
 
 
-    def load_config(self, config_path):
-        with open(config_path) as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
+class GaussianNoise(nn.Module):
+    def __init__(self, opt, device):
+        super(GaussianNoise, self).__init__()
+        self.mean = opt["noise"]["GaussianNoise"]["mean"]
+        self.variance = opt["noise"]["GaussianNoise"]["variance"]
+        self.amplitude = opt["noise"]["GaussianNoise"]["amplitude"]
+        self.p = opt["noise"]["GaussianNoise"]["p"]
+        self.device = device
+
+    def forward(self, encoded, cover_img=None):
+        if random.uniform(0, 1) < self.p:
+            b, c, h, w = encoded.shape
+            Noise = self.amplitude * torch.Tensor(
+                numpy.random.normal(
+                    loc=self.mean, scale=self.variance, size=(b, 1, h, w)
+                )
+            ).to(self.device)
+            Noise = Noise.repeat(1, c, 1, 1)
+            #
+            img_ = Noise + encoded
+
+            return img_
+
+        else:
+            print("Gaussian noise error!")
+            exit()
