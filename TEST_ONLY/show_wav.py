04fdbb9cee19f33d124e15295e6a3bb77fee96e5
PecholaL <pechola.lee@outlook.com>
2023-12-05 17:57:53 +0800

    🍹

--- TEST_ONLY/show_wav.py
+++ TEST_ONLY/show_wav.py
@@ -1,24 +1,13 @@
 import librosa
 import matplotlib.pyplot as plt
-import librosa
-import librosa.display
-import numpy as np
-import sys
+import numpy
 
-host_audio_path = "/Users/pecholalee/Coding/Watermark/miniAWdata/p225_003.wav"
-watermarked_audio_path = "/Users/pecholalee/Coding/Watermark/ideaw_data/output/att_test.wav"
-
-y1, sr1 = librosa.load(host_audio_path, sr=None, mono=True)
-y2, sr2 = librosa.load(watermarked_audio_path, sr=None, mono=True)
-
-f, ((ax11, ax12)) = plt.subplots(1, 2, sharex=False, sharey=False)
-
-ax11.set_title('Host Audio')
-ax11.plot(y1)
+audio_path = "/Users/pecholalee/Coding/Watermark/miniAWdata/p225_003.wav"
 
 
-ax12.set_title('Watermarked Audio')
-ax12.plot(y2)
+y, sr = librosa.load(audio_path, sr=None, mono=True)
+x = numpy.arange(0, len(y))
 
-
+plt.figure(figsize=(5, 5))
+plt.plot(x, y)
 plt.show()
--- TEST_ONLY/test_attack.py
+++ TEST_ONLY/test_attack.py
@@ -20,41 +20,40 @@ d = torch.from_numpy(d).float()
 print(f"audio shape: {d.shape}, audio device: {d.device}")
 
 # GNoise
-d_ = att.gaussianNoise(d)
+d_gn = att.gaussianNoise(d)
 print("Gaussian Noise")
-print(d_.shape, d_.device)
+print(d_gn.shape, d_gn.device)
 
-# # BPass
-# d_ = att.bandpass(d)
-# print("Band Pass")
-# print(d_.shape, d_.device)
+# BPass
+d_bpf = att.bandpass(d)
+print("Band Pass")
+print(d_bpf.shape, d_bpf.device)
 
-# # Dropout
-# d_ = att.dropout(d, d)
-# print("Dropout")
-# print(d_.shape, d_.device)
+# Dropout
+d_do = att.dropout(d, d)
+print("Dropout")
+print(d_do.shape, d_do.device)
 
-# # Resample
-# d_ = att.resample(d)
-# print("Resample")
-# print(d_.shape, d_.device)
+# Resample
+d_rs = att.resample(d)
+print("Resample")
+print(d_rs.shape, d_rs.device)
 
-# # AModify
-# d_ = att.ampMdf(d)
-# print("Amplitude Modify")
-# print(d_.shape, d_.device)
+# AModify
+d_am = att.ampMdf(d)
+print("Amplitude Modify")
+print(d_am.shape, d_am.device)
 
 # Mp3Compress
-# d_ = att.mp3compress(d)
-# print("Mp3 Compress")
-# print(d_.shape, d_.device)
-# d_np = d_.numpy()
+d_mp = att.mp3compress(d)
+print("Mp3 Compress")
+print(d_mp.shape, d_mp.device)
 
+# Time Stretch
+d_ts = att.timeStretch(d)
+print("Time Stretch")
+print(d_ts.shape, d_ts.device)
 
-# # Time Stretch
-# d_ = att.timeStretch(d)
-# print("Time Stretch")
-# print(d_.shape, d_.device)
-
-d_np = d_.numpy()
+# plot
+d_np = d_mp.numpy()
 write("/Users/pecholalee/Coding/Watermark/ideaw_data/output/att_test.wav", 16000, d_np)
